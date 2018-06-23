aaa7ec96c7d24d0561ba9ed676fc1f84c6e0b745
fdai4076 <sebastian.krah2@informatik.hs-fulda.de>
2018-06-23 19:54:31 +0200

    branch = prototyp
    aktuallisieren des eigenen branch = prototyp

--- Prototyp_GAME_Version 0.01/Assets/Script/BombeScript.cs
+++ Prototyp_GAME_Version 0.01/Assets/Script/BombeScript.cs
@@ -2,18 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BombeScript : MonoBehaviour {
 
-	public float power;
-	public float ownerPlayerID;
+public class BombeScript : MonoBehaviour
+{
+    public int bombOwnerPlayerID;
+    public bool remoteBomb = false;
+    public int bombPower = 1;
+    public int bombTimer = 3;
+    private float startTime;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    //Zeitpunkt an dem die Bombe erzeugt wurde
+    void Awake()
+    {
+        startTime = Time.time;
+    }
 
-	// Update is called once per frame
-	void Update () {
-		transform.eulerAngles += new Vector3(0, 50f*Time.deltaTime, 0);
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        transform.eulerAngles += new Vector3(0, 70f * Time.deltaTime, 0);
+
+        if ((bombTimer < (Time.time - startTime)) && !remoteBomb)
+        {
+            CallExplode();
+        }
+    }
+
+    public void CallExplode()
+    {
+        int x = (int)gameObject.transform.position.x;
+        int z = (int)gameObject.transform.position.z;
+
+        Destroy(gameObject);
+        //Debug.Log("MAPDESTROYER EXPLODE WIRD AUFGERUFEN mit Position: " +x+ " / " +z);
+        FindObjectOfType<MapDestroyer>().Explode(x, z);
+    }
 }
--- Prototyp_GAME_Version 0.01/Assets/Script/PlayerScript.cs
+++ Prototyp_GAME_Version 0.01/Assets/Script/PlayerScript.cs
@@ -2,192 +2,176 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerScript : MonoBehaviour {
+public class PlayerScript : MonoBehaviour
+{
 
-	bool moveX = true;
-	bool moveZ = true;
-	GameObject player;
-	public WorldScript World;
-	public GameObject Bombe_Prefab;
-	public GameObject Item_Prefab;
-	float angle = 30f;
+    bool moveX = true;
+    bool moveZ = true;
+    GameObject player;
+    GameObject player2;
+    public WorldScript World;
+    public GameObject Bombe_Prefab;
+    float angle = 30f;
 
-	public int bombCount = 100;
+    public int bombCount = 100;
 
-	// Use this for initialization
-	void Awake () {
-		player = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-		player.transform.localScale = new Vector3(0.8f, 0.8f, 0.8f);
-		player.transform.position = new Vector3(1f, -0.1f, 1f);
-		player.name = "Player 1";
-	}
+    // Use this for initialization
+    void Awake()
+    {
+        player = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        player.transform.localScale = new Vector3(0.8f, 0.8f, 0.8f);
+        player.transform.position = new Vector3(1f, -0.1f, 1f);
+        player.name = "Player 1";
 
-	float speedMultiply = 0.01f;
-	// Update is called once per frame
-	void Update () {
+        //Zu Testzwecken btgl. Camera zweiter Spieler
+        player2 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        player2.transform.localScale = new Vector3(0.8f, 0.8f, 0.8f);
+        player2.transform.position = new Vector3((World.levelBreite - 1) * 2 - 1, -0.1f, (World.levelTiefe - 1) * 2 - 1);
+        player2.name = "Player 2";
+    }
 
-		if((Input.GetKey("a") || Input.GetKey("d") || Input.GetKey("w") || Input.GetKey("s")) && (moveX || moveZ)) {
-			speed();
-			wallTest(player);
-		} else if(speedMultiply > 0.1f) {
-			speedMultiply -= 0.1f;
-			//Debug.Log("Increase SM: " +speedMultiply);
-		} if(speedMultiply < 0.01f) {
-			speedMultiply = 0.01f;
-			//Debug.Log("Setting SM: " +speedMultiply);
-		}
+    float speedMultiply = 0.01f;
+    // Update is called once per frame
+    void Update()
+    {
 
-		if(Input.GetKey("a") && player.transform.position.x > 0) {
-			//whatsNext(1);
-			if(moveX)
-				player.transform.Translate(speedMultiply*-1f*Time.deltaTime,0,0);
-		}
+        if ((Input.GetKey("a") || Input.GetKey("d") || Input.GetKey("w") || Input.GetKey("s")) && (moveX || moveZ))
+        {
+            speed();
+            wallTest(player);
+        }
+        else if (speedMultiply > 0.1f)
+        {
+            speedMultiply -= 0.1f;
+            //Debug.Log("Increase SM: " +speedMultiply);
+        }
+        if (speedMultiply < 0.01f)
+        {
+            speedMultiply = 0.01f;
+            //Debug.Log("Setting SM: " +speedMultiply);
+        }
 
-		if(Input.GetKey("d") && player.transform.position.x < World.levelBreite - 1) {
-			//whatsNext(2);
-			if(moveX)
-				player.transform.Translate(speedMultiply*1f*Time.deltaTime,0,0);
-		}
+        if (Input.GetKey("a") && player.transform.position.x > 0)
+        {
+            //whatsNext(1);
+            if (moveX)
+                player.transform.Translate(speedMultiply * -1f * Time.deltaTime, 0, 0);
+        }
 
-		if(Input.GetKey("w") && player.transform.position.z < World.levelTiefe - 1) {
-			//whatsNext(3);
-			if(moveZ)
-				player.transform.Translate(0,0,speedMultiply*1f*Time.deltaTime);
-		}
+        if (Input.GetKey("d") && player.transform.position.x < World.levelBreite - 1)
+        {
+            //whatsNext(2);
+            if (moveX)
+                player.transform.Translate(speedMultiply * 1f * Time.deltaTime, 0, 0);
+        }
 
-		if(Input.GetKey("s") && player.transform.position.z > 0) {
-			//whatsNext(4);
-			if(moveZ)
-				player.transform.Translate(0,0,speedMultiply*-1f*Time.deltaTime);
-		}
+        if (Input.GetKey("w") && player.transform.position.z < World.levelTiefe - 1)
+        {
+            //whatsNext(3);
+            if (moveZ)
+                player.transform.Translate(0, 0, speedMultiply * 1f * Time.deltaTime);
+        }
 
-		if(Input.GetKey("m")) {
-			//whatsNext(1);
-			Destroy(GameObject.Find("Wand"));
-		}
+        if (Input.GetKey("s") && player.transform.position.z > 0)
+        {
+            //whatsNext(4);
+            if (moveZ)
+                player.transform.Translate(0, 0, speedMultiply * -1f * Time.deltaTime);
+        }
 
-		if(Input.GetKey("b")) {
-			//whatsNext(1);
-			Destroy(GameObject.Find("Kiste"));
-		}
+        if (Input.GetKey("m"))
+        {
+            //whatsNext(1);
+            Destroy(GameObject.Find("Wand"));
+        }
 
-		if(Input.GetKey("space")) {
-			//whatsNext(1);
-			createBomb(player);
-		}
+        if (Input.GetKey("b"))
+        {
+            //whatsNext(1);
+            Destroy(GameObject.Find("Kiste"));
+        }
 
-		if(Input.GetKey("i")) {
-			//whatsNext(1);
-			createItem(player);
-		}
-	}
+        if (Input.GetKeyDown("space"))
+        {
+            //whatsNext(1);
+            createBomb(player);
+        }
 
-	float xPosition;
-	float zPosition;
-	void wallTest(GameObject player) {
-		xPosition = Mathf.Round(player.transform.position.x);
-		zPosition = Mathf.Round(player.transform.position.z);
+    }
 
-		if(World.WorldArray[(int)xPosition,(int)zPosition] != null)
-		{
-			Debug.Log("Object an aktueller Stelle: " +World.WorldArray[(int)xPosition,(int)zPosition]);
-		}	else {
-			Debug.Log("Object an aktueller Stelle: Freier Weg");
-		}
-	}
+    float xPosition;
+    float zPosition;
+    void wallTest(GameObject player)
+    {
+        xPosition = Mathf.Round(player.transform.position.x);
+        zPosition = Mathf.Round(player.transform.position.z);
 
-	void createBomb(GameObject player)
-	{
-		if(World.WorldArray[(int)xPosition,(int)zPosition] == null)
-		{
-			if(bombCount > 0) {
-				bombCount--;
-				GameObject bomb;
-				bomb = Instantiate(Bombe_Prefab, new Vector3(xPosition, -0.1f, zPosition), Quaternion.Euler(0, angle, 0));
-				bomb.name = "Bomb from " +player;
-				World.WorldArray[(int)xPosition,(int)zPosition] = bomb;
-				angle += angle;
+        if (World.WorldArray[(int)xPosition, (int)zPosition] != null)
+        {
+            //Debug.Log("Object an aktueller Stelle: " + World.WorldArray[(int)xPosition, (int)zPosition]);
+            if (World.WorldArray[(int)xPosition, (int)zPosition].name == "Item_SpeedBoost")
+            {
+                Destroy(World.WorldArray[(int)xPosition, (int)zPosition]);
+                speedMultiply = 10f;
+            }
+            if (World.WorldArray[(int)xPosition, (int)zPosition].name == "Item_SpeedLow")
+            {
+                Destroy(World.WorldArray[(int)xPosition, (int)zPosition]);
+                speedMultiply = 0.5f;
+            }
+        }
+        else
+        {
+            //Debug.Log("Object an aktueller Stelle: Freier Weg");
+        }
+    }
 
-				//Destroy(World.WorldArray[(int)xPosition,(int)zPosition], 3f);
-			}
-		}
-	}
+    void createBomb(GameObject player)
+    {
+        if (World.WorldArray[(int)xPosition, (int)zPosition] == null)
+        {
+            if (bombCount > 0)
+            {
+                bombCount--;
 
-	void createItem(GameObject player)
-	{
-		if(World.WorldArray[(int)xPosition,(int)zPosition] == null)
-		{
-			GameObject item;
-			item = Instantiate(Item_Prefab, new Vector3(xPosition, -0.1f, zPosition), Quaternion.Euler(0, angle, 0));
-			item.name = "Item from " +player;
-			World.WorldArray[(int)xPosition,(int)zPosition] = item;
-			angle += angle;
+                GameObject bombeInstanz;
+                bombeInstanz = Instantiate(Bombe_Prefab, new Vector3(xPosition, -0.1f, zPosition), Quaternion.identity);
 
-				//Destroy(World.WorldArray[(int)xPosition,(int)zPosition], 3f);
-		}
-	}
+                BombeScript thisBombeScript = bombeInstanz.GetComponent<BombeScript>();
 
+                thisBombeScript.bombTimer = 3; //WERT MUSS DURCH ITEM ERHÖHT WERDEN
+                thisBombeScript.name = "Bombe"; //BOMBENNAME == PLAYERID. Bombe wird nämlich nur über name gefunden.
+                thisBombeScript.bombOwnerPlayerID = 1;
+        
+                World.WorldArray[(int)xPosition, (int)zPosition] = bombeInstanz;
+                angle += angle;
+            }
+        }
+    }
 
-	// void whatsNext(int richtung)
-	// {
-	// 	if(richtung == 4 && (FindObjectOfType<World>().WorldArray[(int)xPosition,(int)zPosition+1] != null)) {
-	// 		moveZ = false;
-	// 	} else {
-	// 		moveZ = true;
-	// 	}
-	// 	if(richtung == 3 && (FindObjectOfType<World>().WorldArray[(int)xPosition,(int)zPosition-1] != null)) {
-	// 		moveZ = false;
-	// 	} else {
-	// 		moveZ = true;
-	// 	}
-	// 	if(richtung == 2 && (FindObjectOfType<World>().WorldArray[(int)xPosition+1,(int)zPosition] != null)) {
-	// 		moveX = false;
-	// 	} else {
-	// 		moveX = true;
-	// 	}
-	// 	if(richtung == 1 && (FindObjectOfType<World>().WorldArray[(int)xPosition-1,(int)zPosition] != null)) {
-	// 		moveX = false;
-	// 	} else {
-	// 		moveX = true;
-	// 	}
-	// }
+    //GEHT NICHT
+    void remoteExplode()
+    {
 
-		// if(Input.GetKey("a")) {
-		// 	wallTest(player1);
-		// 	bombTest(player1, -1f, 0);
-		// 	if(moveX && noBomb)
-		// 		player1.transform.Translate(-1f,0,0);
-		// 	noBomb = true;
-		// }
-		//
-		// if(Input.GetKey("d")) {
-		// 	wallTest(player1);
-		// 	bombTest(player1, 1f, 0);
-		// 	if(moveX && noBomb)
-		// 		player1.transform.Translate(1f,0,0);
-		// 	noBomb = true;
-		// }
-		//
-		// if(Input.GetKey("w")) {
-		// 	wallTest(player1);
-		// 	bombTest(player1, 0, 1f);
-		// 	if(moveZ && noBomb)
-		// 		player1.transform.Translate(0,0,1f);
-		// 	noBomb = true;
-		// }
-		//
-		// if(Input.GetKey("s")) {
-		// 	wallTest(player1);
-		// 	bombTest(player1, 0, -1f);
-		// 	if(moveZ && noBomb)
-		// 		player1.transform.Translate(0,0,-1f);
-		// 	noBomb = true;
-		// }
+        for (int i = 0; i < World.WorldArray.Length; i++)
+        {
+            for (int j = 0; j < World.WorldArray.Length; j++)
+            {
+                if (World.WorldArray[i, j].gameObject.name == "Bombe")
+                {
+                    Destroy(World.WorldArray[i, j]);
+                }
+            }
+        }
 
-	void speed() {
-		if(speedMultiply < 7.0f) {
-				speedMultiply += (speedMultiply / (speedMultiply * 10.0f));
-				//Debug.Log("Degreace SM: " +speedMultiply);
-		}
-	}
+    }
+
+    void speed()
+    {
+        if (speedMultiply < 5.0f)
+        {
+            speedMultiply += (speedMultiply / (speedMultiply * 10.0f));
+            //Debug.Log("Degreace SM: " +speedMultiply);
+        }
+    }
 }
